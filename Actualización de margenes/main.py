42d9973b7f5c89189cbc9e9565810d19ae07e3e7
TataMarcos <marcos.larran@tata.com.uy>
2025-03-28 14:30:52 -0300

    Cambio en esquema de cierre

--- Actualización de margenes/main.py
+++ Actualización de margenes/main.py
@@ -18,7 +18,7 @@ else:
     cursor = st.session_state.cursor
 
 #Cargamos el archivo
-uploaded_file = st.file_uploader("Cargar el archivo", type="excel")
+uploaded_file = st.file_uploader("Cargar el archivo", type="xlsx")
 
 if uploaded_file is not None:
     mg_update = pd.read_excel(uploaded_file)[['ORIN', 'MARGEN OBJETIVO']].astype({'ORIN':'str'})
@@ -28,31 +28,35 @@ if uploaded_file is not None:
 
     p = st.button('Obtener nuevos márgenes')
     if p:
+        success = False
         df_final = margenes(df_actual=mg_actual, df_update=mg_update)
-        st.dataframe(df_final)
+        st.write('Margenes ingresados')
+        st.dataframe(df_final.dropna())
+        st.write('Margenes actualizados')
+        st.dataframe(df_final[['ORIN', 'MG']])
         sub = st.button('Subir márgenes actualizados')
-    if sub:
-        df_hist = df_final.copy()
-        df_hist['ULTIMA_ACTUALIZACION'] = date.today().strftime('%Y-%m-%d')
-        df_hist['REALIZADA_POR'] = user
-        success = carga_snow_generic(df=df_final, ctx=snow, database='SANDBOX_PLUS', schema='DWH',
-                                     table='INPUT_PRICING_MARGEN_OBJETIVO_HISTORICO')
-        clean_table(cursor=cursor, table='INPUT_PRICING_MARGEN_OBJETIVO')
-        success = carga_snow_generic(df=df_final, ctx=snow, table='INPUT_PRICING_MARGEN_OBJETIVO',
-                                     database='SANDBOX_PLUS', schema='DWH')
-        if success:
-            st.write('Márgenes actualizados')
+        if sub:
+            df_hist = df_final.copy()
+            df_hist['ULTIMA_ACTUALIZACION'] = date.today().strftime('%Y-%m-%d')
+            df_hist['REALIZADA_POR'] = user
+            st.write('Subiendo los datos. No cierre el programa.')
+            success = carga_snow_generic(df=df_final, ctx=snow, database='SANDBOX_PLUS', schema='DWH',
+                                        table='INPUT_PRICING_MARGEN_OBJETIVO_HISTORICO')
+            clean_table(cursor=cursor, table='INPUT_PRICING_MARGEN_OBJETIVO')
+            success = carga_snow_generic(df=df_final, ctx=snow, table='INPUT_PRICING_MARGEN_OBJETIVO',
+                                        database='SANDBOX_PLUS', schema='DWH')
+            if success:
+                st.write('Márgenes actualizados')
 
-    #Armamos bloque para cerrar el programa
-    if success or not(sub):
-        exit_app = st.button("Cerrar el programa.")
-    if exit_app:
-        st.write('Cerrando el programa. Espere 10 segundos antes de cerrar la ventana.')
-        # Give a bit of delay for user experience
-        time.sleep(1)
-        # Terminate streamlit python process
-        pid = os.getpid()
-        p = psutil.Process(pid)
-        p.terminate()
+#Armamos bloque para cerrar el programa
+exit_app = st.button("Cerrar el programa.")
+if exit_app:
+    st.write('Cerrando el programa. Espere 10 segundos antes de cerrar la ventana.')
+    # Give a bit of delay for user experience
+    time.sleep(1)
+    # Terminate streamlit python process
+    pid = os.getpid()
+    p = psutil.Process(pid)
+    p.terminate()
 else:
     st.write('Aún no se cargó el archivo')
